575b99298df191bff783a8709178dfdc742227a6
loudemer <46995022+loudemer@users.noreply.github.com>
2024-02-20 18:51:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,183 @@
+# PVHeatPump
+Cette application Appdaemon pour Home Assistant permet de réguler les pompes à chaleur air-eau et plus particulièrement celles qui utilisent un plancher chauffant. On peut aussi l’utiliser avec des circuits de chauffage central. 
+
+Ce mode de chauffage par PAC a pour particularité d’avoir une forte inertie qui lui confère à la fois des avantages tels que la mise en route du chauffage de nuit en heures creuses pour se chauffer le jour ce qui permet de faire des économies mais aussi des inconvénients en termes de régulation car il faut anticiper la chauffe et de montée en température qui se fait lentement sur plusieurs heures. 
+
+Les PAC ont une régulation interne, prenant en compte la température intérieure et la température extérieure pour assurer la régulation de la température en suivant la courbe d’eau. Elles fonctionnement souvent avec une horloge de fonctionnement et une sonde thermostatique intérieure.
+
+Ce logiciel vient remplacer le thermostat intérieur qui travaille sur un switch dans la PAC. Il permet d’optimiser la dépense énergétique en fonctionnant en heures creuses et si besoin, dans la journée au moment où la production solaire le permet. 
+Pour cela il est couplé à l’application PVOptimizer qui distribue l’énergie solaire à tous les gros consommateurs de la maison.
+
+Il est possible de faire fonctionner cette application de manière autonome sans PVOptimizer. Le mode de fonctionnement 3 décrit ci-dessous n’est alors pas disponible
+# Fonctionnement
+L’application utilise trois modes de fonctionnement qui peuvent se compléter :
+
+1. Un fonctionnement nocturne classique pendant les heures creuses. Le logiciel s’occupe alors de mettre en route la PAC en heures creuses lorsque les températures intérieures et extérieure deviennent basses. 
+   Il s’occupe aussi d’arrêter la PAC lorsque la température de consigne intérieure est atteinte ou lorsque l’on bascule en heures pleines
+1. Un fonctionnement nocturne limité (marche forcée) qui concerne les périodes où les températures extérieures ne sont pas trop basses ne nécessitant alors pas un chauffage toute la nuit. 
+Le logiciel met en route la PAC, une ou plusieurs heures en fin de nuit avant la bascule en heures pleines ce qui permet de chauffer la maison de manière optimale en gardant un maximum d’énergie pour la journée. Ce mode de fonctionnement est alternatif au premier. 
+   Pour calculer le nombre d’heures de chauffe nécessaires, le logiciel prend en compte les températures extérieures et intérieures, ainsi que la prévision de température le lendemain matin. Il est prévu de prendre en compte la prévision de production solaire mais celle ci est pour l'instant peu fiable.
+1. Un fonctionnement diurne que l’on qualifiera de complémentaire dans les périodes de grand froid qui va se faire en plus du fonctionnement nocturne au moment où la production d’énergie solaire est maximale. Ce troisième mode est piloté par l’application : PVOptimizer. 
+   La demande de mise en route se fait sur le dasboard de PVOptimizer
+
+Le choix entre les modes 1 et 2 est effectuée par le logiciel en fonction des températures intérieures et extérieures le soir à 21h30.
+On peut sélectionner soi-même le mode 1 ou 2 si besoin.
+
+# Prérequis
+- Installation de appdaemon
+- Installation de PVOptimiseur
+- Sensor relai PAC (en remplacement du contacteur du thermostat d’ambiance)
+- Couleur du jour pour les abonnements tempo 
+On peut utiliser l’API RTE par exemple. <https://www.api-couleur-tempo.fr/api>
+- Sensor Heures Creuses
+Disponible dans l’API RTE
+- Prévision de la température du lendemain
+- Sensor température intérieure 
+(A placer dans une pièce éloignée de la cheminée s’il y en a une)
+- Sensor de consigne confort ou réduit (absence prolongée)
+- Sensor température de consigne
+# Installation
+1. Installer **l’add-on appdaemon** à partir de paramètres / modules complémentaires si cela n’est pas déjà fait.
+1. **Télécharger le dépôt**[https://github.com/loudemer/PVHeatPump.git]
+1. Mettre les fichiers *PVHeatPump.py et PVHeatPump.yaml* dans le répertoire *addon\_configs/a0d7b954\_appdaemon/apps*
+1. Mettre le fichier *optimizerentities.yaml* dans le répertoire */config/* de HA ou dans un sous répertoire dédié au fichiers yaml si vous en avez un.
+# Configuration
+1. Ajouter dans le fichier `addon_configs/a0d7b954_appdaemon/appdaemon.yaml`
+```   
+      pvheatpump_log :
+        name: PVHeatPumpLog
+        filename: /homeassistant/log/pvheatpump.log
+        log_generations: 3
+        log_size: 100000
+```
+   Ceci vous permet de lire les log de l’application dans le fichier /config/ pvheatpump\_log  ou directement dans http://ip\_ha:5050
+1. **Compléter** le fichier `/addon_configs/a0d7b954_appdaemon/apps/PVHeatPump.yaml` :
+
+Certains sensors sont optionnels : `heat_pump_water_temperature`, `grid_power_online`.
+
+Si vous n'utilisez pas PVOptimizer les sensors `enable_solar_optimizer`, `start_heat_pump`sont aussi optionnels
+Il faut alors mettre un sensor vide : "" à la place.
+
+Pour les autres, il faut créer les sensors qui ne sont pas déjà présents dans votre configuration.
+```
+PVHeatPump:
+  class: PVHeatPump
+  module: PVHeatPump
+  log: pvheatpump_log
+  # Arret / Marche de l'application PAC
+  constrain_input_boolean: input_boolean.enable_heating
+
+  # temperature interieure
+  inside_temperature: sensor.temperature_cellier
+  # temperature exterieure
+  outside_temperature: sensor.temperature_toit
+  # temperature demandee
+  threshold_temperature: input_number.chauffage_demande
+  # mode confort ou reduit (absencer)
+  comfort_heating: input_boolean.chauffage_confort
+
+  # switch commande PAC
+  heat_pump_command: switch.relai_pac
+  # temperature circuit eau PAC (optionnel)
+  heat_pump_water_temperature: sensor.temperature_pac
+
+  # Couleur tempo du jour
+  current_tempo_color: sensor.rte_tempo_couleur_actuelle
+  # Couleur tempo du lendemain
+  tomorrow_tempo_color: sensor.rte_tempo_prochaine_couleur
+  # indicateur HP / HC
+  off_peak: binary_sensor.rte_tempo_heures_creuses
+
+  # Marche forcee (mode 2) 
+  forced_mode: input_boolean.marche_forcee_pac
+  # Duree marche forcee
+  forced_mode_max_duration: input_number.duree_max_marche_forcee_pac
+  # Prevision temperature lendemain matin
+  tomorrow_morning_temperature: sensor.temperature_forecast_next_day
+
+  # Prevision ensoleillement lendemain 
+  # tomorrow_solar_energy_forecast : sensor.energy_production_tomorrow
+  # Indicateur coupure de courant (onduleur) (Optionnel)
+  grid_power_online: sensor.ups_status
+
+  # Mise en route de la PAC par PVOptimizer : input_boolean.start_device_x
+  # (x etant le rang de l'appareil dans la liste commencant par 1)
+  # (optionnel)
+  start_heat_pump: input_boolean.start_device_4
+  # Application PVOptimizer disponible (optionnel)
+  enable_solar_optimizer: input_boolean.enable_solar_optimizer
+  # la demande de mise en route journée se fait directement sur PVOptimizer
+  # demande_pac: input_boolean.device_request_x 
+
+```
+
+# Le Dashboard
+A titre d’exemple vous trouverez dans le dépôt un fichier yaml d’exemple de configuration de base tout à fait perfectible.
+```
+type: vertical-stack
+title: PAC
+cards:
+  - type: horizontal-stack
+    cards:
+      - type: custom:mushroom-entity-card
+        entity: switch.relai_pac
+        tap_action:
+          action: more-info
+        hold_action:
+          action: toggle
+        icon_color: yellow
+        layout: vertical
+        name: Activation
+        primary_info: name
+        secondary_info: state
+      - type: custom:mushroom-entity-card
+        entity: input_boolean.marche_forcee_pac
+        icon_color: yellow
+        layout: vertical
+        tap_action:
+          action: toggle
+        hold_action:
+          action: more-info
+        name: Marche forcée
+        primary_info: name
+        secondary_info: state
+      - type: custom:mushroom-entity-card
+        entity: sensor.temperature_pac
+        layout: vertical
+        name: Temp eau PAC
+        primary_info: name
+        tap_action:
+          action: more-info
+  - type: horizontal-stack
+    cards:
+      - type: custom:mushroom-number-card
+        entity: input_number.duree_max_marche_forcee_pac
+        name: Durée M. forcée
+        icon: mdi:battery-clock-outline
+        icon_color: yellow
+        display_mode: buttons
+      - type: custom:mushroom-number-card
+        entity: input_number.chauffage_demande
+        name: Temp demandée
+        icon_color: accent
+        icon: mdi:temperature-celsius
+        primary_info: name
+        display_mode: buttons
+        tap_action:
+          action: more-info
+
+```
+
+# Mode d’emploi
+Une fois l’installation réalisée, l’intégration est opérationnelle.
+
+## Visualisation des problèmes
+L’intégration génère un fichier de log qui est stocké dans le fichier `/config/log/pvheatpump.log`.
+Il est possible aussi d’avoir plus de détails en appelant directement la console de debug d’appdaemon : [http://<ip_homeassistant>:5050]()
+
+Vous pourrez alors voir le démarrage et l’arrêt de l’intégration dans *main_log*, les erreurs éventuelles dans *error_log* et le déroulement de l’activité de l’intégration dans *pvheatpump_log*.
+# Désinstallation
+Il faut retirer les 2 fichiers *PVHeatPump.py et PVHeatPump.py.yaml* dans le répertoire *addon\_configs/a0d7b954\_appdaemon/apps/*
+Retirer aussi le dashboard PVHeatPump
+Redémarrer HA.
+
---
+++ apps/PVHeatPump/PVHeatPump.py
@@ -0,0 +1,304 @@
+##############################################################################################
+# PVHeatPump
+# Author: Gerard Mamelle (2024)
+# Version : Beta
+# Program under MIT licence
+##############################################################################################import hassapi as hass
+import math
+import datetime
+from datetime import timedelta
+
+OUTSIDE_THRESHOLD_TEMPERATURE = 15.0
+COMFORT_THRESHOLD_TEMPERATURE = 20.5
+REDUCED_THRESHOLD_TEMPERATURE = 19.5
+FORCED_MAX_DURATION = 3
+
+class PVHeatPump(hass.Hass):
+
+    def initialize(self):
+        self.start_inside_threshold_temperature = self.get_safe_float("threshold_temperature")
+        self.comfort_heating = self.get_state(self.args["comfort_heating"])
+        self.tomorrow_morning_temperature = self.get_safe_float("tomorrow_morning_temperature")
+        #self.tomorrow_solar_energy_forecast = self.get_safe_float("tomorrow_solar_energy_forecast ")
+        self.off_peak = self.get_state(self.args["off_peak"])
+        self.grid_power_online = self.get_state(self.args["grid_power_online"])
+        self.start_heat_pump = 'off'
+        self.set_state(self.args["start_heat_pump"],state='off')
+        
+        duree = self.get_safe_float("forced_mode_max_duration")
+        if duree != None:
+            self.forced_max_duration = int(duree)
+        else:
+            self.forced_max_duration = FORCED_MAX_DURATION
+        self.forced_mode = self.get_state(self.args["forced_mode"])
+        self.forced_mode_start_time = self.datetime() + timedelta(hours=24)
+        self.timer = None
+        
+        # Init listen for pool events
+        self.listen_state(self.change_inside_temperature,self.args["inside_temperature"])
+        self.listen_state(self.change_outside_temperature,self.args["outside_temperature"])
+        self.listen_state(self.change_off_peak,self.args["off_peak"])
+        self.listen_state(self.change_comfort_heating,self.args["comfort_heating"])
+        self.listen_state(self.change_tempo_color,self.args["current_tempo_color"])                   
+        self.listen_state(self.change_forced_mode,self.args["forced_mode"])                   
+        self.listen_state(self.change_forced_mode_duration,self.args["forced_mode_max_duration"])   
+        self.listen_state(self.change_threshold_temperature,self.args["threshold_temperature"])          
+        if self.args["enable_solar_optimizer"] == "":
+            self.enable_solar_optimizer = "off"
+        else:
+            self.enable_solar_optimizer = self.get_state(self.args["enable_solar_optimizer"])
+            self.listen_state(self.change_enable_solaroptimizer,self.args["enable_solar_optimizer"])  
+            self.listen_state(self.change_start_heating,self.args["start_heat_pump"])                          
+        if self.args["grid_power_online"] != "":
+            self.listen_state(self.change_grid_power_online,self.args["grid_power_online"])
+        
+        self.run_daily(self.check_forced_mode, "01:00:00")
+        self.run_daily(self.optimize_heating, "21:30:00")
+
+        #set tempo day
+        self.current_tempo_color = self.get_state(self.args["current_tempo_color"])
+        self.tomorrow_tempo_color = self.get_state(self.args["tomorrow_tempo_color"])
+        # Set temperature de reference
+        self.set_temperature_reference()
+        # Try to turn on HP
+        self.check_HP()
+        # Initialisation terminee 
+        self.log('PVHeatPump initialized')
+    
+    @property
+    def inside_temperature(self) -> float:
+        try:
+            return float(self.get_safe_float("inside_temperature"))
+        except Exception as e:
+            self.log("Error getting inside temperature", e)
+            return None
+
+    @property
+    def outside_temperature(self) -> float:
+        try:
+            return float(self.get_safe_float("outside_temperature"))
+        except Exception as e:
+            self.log("Error getting outside temperature", e)
+            return None
+
+    def get_safe_float(self, entity_id: str):
+        state = self.get_state(self.args[entity_id])
+        if not state or state == "unknown" or state == "unavailable":
+            return None
+        float_val = float(state)
+        return None if math.isinf(float_val) or not math.isfinite(float_val) else float_val
+
+    # Change tempo color
+    def change_tempo_color(self, entity, attribute, old_state, new_state, kwargs):
+        self.log(f'Day tempo color = {new_state}')
+        self.current_tempo_color = new_state
+        self.check_HP()
+
+    # Change off peak 
+    def change_off_peak(self, entity, attribute, old_state, new_state, kwargs):
+        self.log(f'Off peak = {new_state}')
+        self.off_peak = new_state
+        # self.log_data()
+        self.check_HP()
+    
+    # Change optimizer status
+    def change_enable_solaroptimizer(self, entity, attribute, old_state, new_state, kwargs):
+        self.log(f'Solar optimizer = {new_state}')
+        self.enable_solar_optimizer = new_state
+        self.check_HP()
+    
+    # Change start status from optimizer
+    def change_start_heating(self, entity, attribute, old_state, new_state, kwargs):
+        self.log(f'Start HP = {new_state}')
+        self.start_heat_pump = new_state 
+        self.check_HP()
+     
+    # Power failure, we stop heat pump
+    def change_grid_power_online(self, entity, attribute, old_state, new_state, kwargs):
+        if new_state == 'Online' :
+            self.grid_power_online = 'Online'
+            self.check_HP()            
+            self.log("Grid power on")
+        else:
+            if new_state == 'On Battery' :
+                self.grid_power_online = 'On Battery'
+                self.turn_off(self.args['heat_pump_command'])
+                self.log("Power failure, heat pump off")
+    
+    def change_forced_mode_duration(self, entity, attribute, old_state, new_state, kwargs):
+        forced_mode_duration= self.get_safe_float("duree_max_forced_mode")
+        if forced_mode_duration!= None:
+            self.forced_max_duration = int(forced_mode_duration)
+         
+    def change_forced_mode(self, entity, attribute, old_state, new_state, kwargs):
+        self.forced_mode = new_state
+        if self.forced_mode == 'off':
+            self.check_HP()
+        else:
+            self.forced_mode_start_time = self.datetime() + timedelta(hours = 24)
+    
+    def change_threshold_temperature(self, entity, attribute, old_state, new_state, kwargs):
+        self.start_inside_threshold_temperature = self.get_safe_float("threshold_temperature")
+        self.check_HP()
+
+    # definition de la temperature de declenchement de la HP en fonction de la comfort_heating
+    def set_temperature_reference(self):
+        if self.comfort_heating == 'on':
+            self.start_inside_threshold_temperature = COMFORT_THRESHOLD_TEMPERATURE
+        else:
+            self.start_inside_threshold_temperature = REDUCED_THRESHOLD_TEMPERATURE
+        self.log(f'Temperature de declenchement = {self.start_inside_threshold_temperature}')
+
+    def change_inside_temperature(self, entity, attribute, old_state, new_state, kwargs):
+        if self.inside_temperature == None:
+            return
+        self.check_HP()
+    
+    def change_outside_temperature(self, entity, attribute, old_state, new_state, kwargs):
+        if self.outside_temperature == None:
+            return
+        self.check_HP()
+
+    def change_comfort_heating(self, entity, attribute, old_state, new_state, kwargs):
+        self.comfort_heating = new_state
+        self.set_temperature_reference()
+        self.log(f'comfort_heating = {self.comfort_heating}')
+        self.check_HP()
+
+    def do_check_HP(self,kwargs):
+        self.check_HP()
+
+    # Mise en route marche forcee 
+    def check_forced_mode(self,kwargs):
+        if self.forced_mode == 'off':
+            self.forced_mode_start_time = self.datetime() + timedelta(hours = 24)
+        else:
+            delai = 5 - self.forced_max_duration
+            self.forced_mode_start_time = self.datetime() + timedelta(hours = delai)
+            self.log(f'start time = {self.forced_mode_start_time}')
+            if self.timer != None: 
+                self.cancel_timer(self.timer)
+            self.timer = self.run_at(self.do_check_HP, self.forced_mode_start_time)
+            self.check_HP()
+    
+    # Optimisation de la duree de fonctionnement de la HP
+    def optimize_heating(self, kwargs):
+        self.tomorrow_morning_temperature = self.get_safe_float("tomorrow_morning_temperature")
+        self.tomorrow_solar_energy_forecast = self.get_safe_float("tomorrow_solar_energy_forecast")
+        self.tomorrow_tempo_color = self.get_state(self.args["tomorrow_tempo_color"])
+        # Conditions et duree de mise en route marche forcee
+        if (self.inside_temperature > (self.start_inside_threshold_temperature - 1.0)
+            and self.tomorrow_morning_temperature > 5.0
+            and self.tomorrow_tempo_color != "Rouge"):
+            self.forced_mode = 'on'
+            if self.tomorrow_morning_temperature < 0.0:
+                self.forced_max_duration = 5
+            elif self.tomorrow_morning_temperature < 7.0:
+                self.forced_max_duration = 4
+            elif self.tomorrow_morning_temperature < 10.0:
+                self.forced_max_duration = 3
+            else:
+                self.forced_max_duration = 2
+            # si la journée du lendemain est ensoleillee on prévoit de chauffer dans la journée
+            # if self.tomorrow_solar_energy_forecast > 15.0:
+            #    self.forced_max_duration = self.forced_max_duration - 1            
+            self.set_state(self.args['forced_mode_max_duration'], state = float(self.forced_max_duration))
+            self.set_state(self.args['forced_mode'], state = 'on')
+        else:
+            self.forced_mode = 'off'
+            self.set_state(self.args['forced_mode'], state = 'off')
+        self.log(f'Forced mode = {self.forced_mode}')
+
+
+    # Heat pump regulation function
+    def check_HP(self):
+        try:
+            status_HP = self.get_state(self.args["heat_pump_command"])
+        except KeyError:
+            self.log(f'Could not get status HP : {status_HP}')
+            return
+        outside_temperature = self.outside_temperature
+        inside_temperature = self.inside_temperature
+        if inside_temperature == None or outside_temperature == None:
+            return
+
+        # forced mode Regulation
+        if self.forced_mode == 'on':
+            if self.off_peak == 'off':
+                if status_HP == "on":
+                    self.turn_off(self.args['heat_pump_command'])
+                    #self.log_data()
+                    self.log("Stop HP, Off peak off")
+                    self.set_state(self.args['forced_mode'], state = 'off')
+                    self.forced_mode_start_time = self.datetime() + timedelta(hours = 24)
+           # Mise en route forcee de la HP 
+            if self.forced_mode_start_time < self.datetime():
+                if status_HP == "off":
+                    self.turn_on_HP()                    
+                    #self.log_data()
+                    self.log("Start HP")
+            return     
+
+        # Night Regulation
+        if self.off_peak == 'on':
+            if (inside_temperature <= self.start_inside_threshold_temperature 
+                and outside_temperature < OUTSIDE_THRESHOLD_TEMPERATURE):
+                if status_HP == "off":
+                    self.turn_on_HP()
+                    #self.log_data()
+                    self.log("Night Auto start HP")
+                    return
+            if status_HP == "on" and inside_temperature > self.start_inside_threshold_temperature:
+                self.turn_off(self.args['heat_pump_command'])
+                #self.log_data()
+                self.log("Night auto stop HP")
+            return
+
+        # Day Regulation
+        if self.enable_solar_optimizer == 'on':
+            if self.start_heat_pump == 'on':
+                if (inside_temperature > self.start_inside_threshold_temperature 
+                    or outside_temperature > OUTSIDE_THRESHOLD_TEMPERATURE) :
+                    if status_HP == "off" and not self.current_tempo_color == 'Rouge' :
+                        #self.log_data()
+                        self.turn_on_HP()                   
+                        self.log("Start HP (PVOptimizer)")
+            else: # start HP off
+                if status_HP == "on":
+                    self.turn_off(self.args['heat_pump_command'])
+                    #self.log_data()
+                    self.log("Stop HP (PVOptimizer)")
+        
+    #Start HP and Check if HP is running well
+    def turn_on_HP(self):
+        self.turn_on(self.args['heat_pump_command'])
+        if self.args['heat_pump_water_temperature'] != "":
+            self.run_in(self.check_temperature, 30*60)
+
+    def start_heating(self, kwargs):
+        self.turn_on_HP()
+
+    # Check if HP water temperature is increasing and notify if not
+    def check_temperature(self, kwargs):
+        temperature = self.get_safe_float("heat_pump_water_temperature")
+        if temperature == None:
+            return
+        if temperature < 24.0:
+            self.call_service("notify/mobile_app_iphone_gerard",message="HP not running well")
+            self.log("HP not running well")
+
+    def getdifference_minutes(self,recent, old):
+        diff = recent - old
+        minutes = diff.total_seconds() / 60
+        return int(minutes)
+    
+    # Log data
+    def log_data(self):
+        self.log(f'status Off peak = {self.off_peak}')
+        self.log(f'status PVOptimizer = {self.enable_solar_optimizer}')
+        self.log(f'status Onduleur = {self.grid_power_online}')
+        self.log(f'comfort_heating = {self.comfort_heating}')
+        self.log(f'Temperature cellier = {self.inside_temperature}')
+        self.log(f'Temperature exterieure = {self.outside_temperature}')
+        self.log(f'Tempo color = {self.current_tempo_color}')
+
